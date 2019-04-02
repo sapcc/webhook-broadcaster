238867bd21a067986d5d625483dd93d1171e6fdf
Fabian Ruff <fabian@progra.de>
2019-04-02 14:33:24 +0200

    fixes for pieplines in multiple teams
    We have to iterate of all teams to get all the pipelines

--- resource_cache.go
+++ resource_cache.go
@@ -24,55 +24,64 @@ var (
 
 func UpdateCache(client concourse.Client) error {
 
-	pipelines, err := client.ListPipelines()
+	teams, err := client.ListTeams()
 	if err != nil {
-		return fmt.Errorf("Failed to list pipelines: %s", err)
+		return fmt.Errorf("Failed to list teams: %s", err)
 	}
+	pipelinesByID := make(map[int]atc.Pipeline, 50)
+	for _, team := range teams {
+		client := client.Team(team.Name)
+		pipelines, err := client.ListPipelines()
+		if err != nil {
+			return fmt.Errorf("Failed to list pipelines: %s", err)
+		}
+		log.Printf("Processing %d pipeline(s) for team %s", len(pipelines), team.Name)
 
-	pipelinesByID := make(map[int]atc.Pipeline, len(pipelines))
-	for _, pipeline := range pipelines {
-		pipelinesByID[pipeline.ID] = pipeline
+		//temporarly memorize pipelines from team to cleanup after the teams loop
+		for _, pipeline := range pipelines {
+			pipelinesByID[pipeline.ID] = pipeline
+		}
+
+		//update pipeline cache
+		for _, pipeline := range pipelines {
+			config, _, version, found, err := client.PipelineConfig(pipeline.Name)
+			if err != nil {
+				log.Printf("Failed to get pipeline %s/%s: %s", pipeline.TeamName, pipeline.Name, err)
+				continue
+			}
+			if found {
+				cachedPipeline, inCache := resourceCache.Load(pipeline.ID)
+				//add or replace cache for pipeline
+				if !inCache || cachedPipeline.(Pipeline).Version != version {
+					newCacheObj := Pipeline{
+						ID:      pipeline.ID,
+						Name:    pipeline.Name,
+						Team:    pipeline.TeamName,
+						Version: version,
+					}
+					for _, resource := range config.Resources {
+						//Skip resources without webhook tokens
+						if resource.WebhookToken == "" {
+							continue
+						}
+						newCacheObj.Resources = append(newCacheObj.Resources, resource)
+					}
+					resourceCache.Store(pipeline.ID, newCacheObj)
+					log.Printf("New version detected for pipeline %s/%s. Found %d resource(s) that have a webhook token.", pipeline.TeamName, pipeline.Name, len(newCacheObj.Resources))
+				}
+			}
+		}
 	}
-
 	//delete removed pipelines from cache
-	resourceCache.Range(func(key, _ interface{}) bool {
+	resourceCache.Range(func(key, value interface{}) bool {
 		pipelineID := key.(int)
-		if cachedPipeline, found := pipelinesByID[pipelineID]; !found {
-			log.Printf("Removing pipeline %s/%s from cache", cachedPipeline.TeamName, cachedPipeline.Name)
+		cachedPipeline := value.(Pipeline)
+		if _, found := pipelinesByID[pipelineID]; !found {
+			log.Printf("Removing vanished pipeline %s/%s from cache", cachedPipeline.Team, cachedPipeline.Name)
 			resourceCache.Delete(pipelineID)
 		}
 		return true
 	})
-
-	//update pipeline cache
-	for _, pipeline := range pipelines {
-		config, _, version, found, err := client.Team(pipeline.TeamName).PipelineConfig(pipeline.Name)
-		if err != nil {
-			log.Printf("Failed to get pipeline %s/%s: %s", pipeline.TeamName, pipeline.Name, err)
-			continue
-		}
-		if found {
-			cachedPipeline, inCache := resourceCache.Load(pipeline.ID)
-			//add or replace cache for pipeline
-			if !inCache || cachedPipeline.(Pipeline).Version != version {
-				newCacheObj := Pipeline{
-					ID:      pipeline.ID,
-					Name:    pipeline.Name,
-					Team:    pipeline.TeamName,
-					Version: version,
-				}
-				for _, resource := range config.Resources {
-					//Skip resources without webhook tokens
-					if resource.WebhookToken == "" {
-						continue
-					}
-					newCacheObj.Resources = append(newCacheObj.Resources, resource)
-				}
-				resourceCache.Store(pipeline.ID, newCacheObj)
-				log.Printf("New version detected for pipeline %s/%s. Found %d resource(s) that have a webhook token.", pipeline.TeamName, pipeline.Name, len(newCacheObj.Resources))
-			}
-		}
-	}
 	return nil
 }
 
