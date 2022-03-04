314e98ac1084738ca31ebdfb30c5432cdb9c91f9
Fabian Ruff <fabian.ruff@sap.com>
2022-03-04 14:02:54 +0100

    Consider resource path filter before notifying
    This change will not notify resources if the list of changed files is not matched by the resources path filter.

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 PKG:=github.com/sapcc/webhook-broadcaster
 IMAGE:=sapcc/concourse-webhook-broadcaster
-VERSION:=0.7.0
+VERSION:=0.8.0
 build:
 	go build -v -o bin/webhook-broadcaster $(PKG)
 
 docker:
 	go test -v
-	GOOS=linux CGO_ENABLED=0 go build -o bin/linux/webhook-broadcaster $(PKG)
+	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/linux/webhook-broadcaster $(PKG)
 	docker build -t $(IMAGE):$(VERSION) .
 
 push:
--- handlers.go
+++ handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/concourse/concourse/atc"
 )
@@ -20,7 +22,14 @@ func (gh *GithubWebhookHandler) ServeHTTP(rw http.ResponseWriter, req *http.Requ
 			FullName string `json:"full_name"`
 			CloneURL string `json:"clone_url"`
 			GitURL   string `json:"git_url"`
-		}
+		} `json:"repository"`
+		Commits []struct {
+			ID            string   `json:"id"`
+			Message       string   `json:"message"`
+			AddedFiles    []string `json:"added"`
+			RemovedFiles  []string `json:"removed"`
+			ModifiedFiles []string `json:"modified"`
+		} `json:"commits"`
 	}
 	if req.Body == nil {
 		rw.WriteHeader(400)
@@ -36,12 +45,24 @@ func (gh *GithubWebhookHandler) ServeHTTP(rw http.ResponseWriter, req *http.Requ
 
 	log.Printf("Received webhhook for %s", pushEvent.Repository.CloneURL)
 
+	//collect list of changed files
+	filesChanged := []string{}
+	for _, commit := range pushEvent.Commits {
+		filesChanged = append(filesChanged, commit.AddedFiles...)
+		filesChanged = append(filesChanged, commit.RemovedFiles...)
+		filesChanged = append(filesChanged, commit.ModifiedFiles...)
+	}
+
 	ScanResourceCache(func(pipeline Pipeline, resource atc.ResourceConfig) bool {
 		if resource.Type != "git" && resource.Type != "pull-request" && resource.Type != "git-proxy" {
 			return true
 		}
 		if uri, ok := resource.Source["uri"].(string); ok {
 			if SameGitRepository(uri, pushEvent.Repository.CloneURL) {
+				if paths, ok := resource.Source["paths"].([]string); ok && len(paths) > 1 && !matchFiles(paths, filesChanged) {
+					log.Printf("Skipping resource %s due to path filter", resource.Name)
+					return true
+				}
 				webhookURL := fmt.Sprintf("%s/api/v1/teams/%s/pipelines/%s/resources/%s/check/webhook?webhook_token=%s",
 					concourseURL,
 					pipeline.Team,
@@ -55,4 +76,28 @@ func (gh *GithubWebhookHandler) ServeHTTP(rw http.ResponseWriter, req *http.Requ
 		return true
 	})
 
+}
+
+func matchFiles(patterns []string, files []string) bool {
+	for _, file := range files {
+		for _, pattern := range patterns {
+			// direct match
+			if file == pattern {
+				return true
+			}
+			// directory match
+			if strings.HasSuffix(pattern, "/") && strings.HasPrefix(file, pattern) {
+				return true
+			}
+			// directory without trainling / match
+			if strings.HasPrefix(file, pattern+"/") {
+				return true
+			}
+			//last resort glob match
+			if ok, _ := filepath.Match(pattern, file); ok {
+				return true
+			}
+		}
+	}
+	return false
 }
