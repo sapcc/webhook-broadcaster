0793f7af1501964b3a1bcc29e7131e079db34df2
Jan Knipper <jan.knipper@sap.com>
2025-02-17 14:42:51 +0100

    Remove entrypoint script

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/webhook-broadcaster -concourse-url "${CONCOURSE_URL}" \
-    -auth-user "${CONCOURSE_USER}" \
-    -auth-password "${CONCOURSE_PASSWORD}" \
-    -refresh-interval "${CONCOURSE_REFRESH_INTERVAL:-5m}" \
-    -listen-addr "${CONCOURSE_LISTEN_ADDRESS:-:8080}" \
-    -webhook-concurrency "${CONCOURSE_WEBHOOK_CONCURRENCY:-20}"
